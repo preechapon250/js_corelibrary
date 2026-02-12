8cbe8c54cef00fe1798f5d89fb9d170fcb1979fa
Kamil Golec <kumalgfilms@gmail.com>
2026-02-12 13:22:43 +0100

    chore: remove unnecessary typedefs

--- packages/nx-plugins/src/index.js
+++ packages/nx-plugins/src/index.js
@@ -1,10 +1,3 @@
-/**
- * @typedef {import("./contracts.js").ContractsPluginOptions} ContractsPluginOptions
- * @typedef {import("./intl.js").IntlPluginOptions} IntlPluginOptions
- * @typedef {import("./proxy.js").ProxyPluginOptions} ProxyPluginOptions
- * @typedef {import("./publish.js").PublishPluginOptions} PublishPluginOptions
- */
-
 export { createNodesV2 as contractsPlugin } from "./contracts.js"
 export { createNodesV2 as intlPlugin } from "./intl.js"
 export { createNodesV2 as proxyPlugin } from "./proxy.js"
